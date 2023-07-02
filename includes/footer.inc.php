e61a484fdbfa6f13205c4c91404e71c8a39eeb6d
Sukanya Madbhavi <87230542+sukanyamadbhavi@users.noreply.github.com>
2023-07-02 22:53:02 +0530

    Add files via upload

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,3 +1,3 @@
 <!-- <div id="footer-wrap">
-	<p id="legal">(c) 2010 OurSite. Design by <a href="http://www.phptpoint.com"> PHP-t-POINT</a>.</p>
+	<p id="legal">Web Technology Miniproject</p>
 	</div> -->
--- includes/search.inc.php
+++ includes/search.inc.php
@@ -1,32 +1,7 @@
 <ul>
-			<li id="login">
-				
-						<?php
-						require('includes/config.php');
-							if(isset($_SESSION['status']))
-							{
-								echo '<h2>Hello :  '.$_SESSION['unm'].'</h2>';
-							}
-							else
-							{
-								echo '<form action="process_login.php" method="POST">
-										<h2>LogIn</h2>
-											<b>Username:</b>
-											<br><input type="text" name="usernm"><br>
-											<br>
-											
-											
-											<b>Password:</b>
-											<br><input type="password" name="pwd">
-											<input type="submit" id="x" value="Login" />
-										</form>';
-							}
-						?>
-			</li>
-
 			<li id="search">
 				<h2>Search</h2>
-				<form method="GET" action="search_result.php">
+				<form method="get" action="">
 					<fieldset>
 					<input type="text" id="s" name="s" value="" />
 					<input type="submit" id="x" value="Search" />
@@ -36,36 +11,14 @@
 			<li>
 				<h2>Categories</h2>
 				<ul>
-					
-					
-					<!--
+					<li><a href="#">Computer</a></li>
+					<li><a href="#">History</a></li>
+					<li><a href="#">Travelling</a></li>
 					<li><a href="#">Economics</a></li>
-					<li><a href="#">Fiction</a></li>
-					<li><a href="#">Forestry & WildLife</a></li>
-					<li><a href="#">Health & Physics</a></li>
-					<li><a href="#">Historical</a></li>
-					<li><a href="#">Social</a></li>
-					<li><a href="#">Sports & Physical Education</a></li>
-					<li><a href="#">Terrorism</a></li>
-					<li><a href="#">Tourism</a></li>
-					<li><a href="#">Tracking </a></li>
-					<li><a href="#">Yoga</a></li>
-					-->
-								<?php
-										
-			
-										$query="select * from category ";
-			
-										$res=mysqli_query($conn,$query);
-											
-										while($row=mysqli_fetch_assoc($res))
-											{
-												echo '<li><a href="subcat.php?cat='.$row['cat_id'].'&catnm='.$row["cat_nm"].'">'.$row["cat_nm"].'</a></li>';
-												//pass catid not catnm
-											}
-			
-											mysqli_close($conn);
-								?>
+					<li><a href="#">Comics</a></li>
+					<li><a href="#">Religion</a></li>
+					<li><a href="#">Commerce</a></li>
+					<li><a href="#">Science</a></li>
 				</ul>
 			</li>
 			
